dd2c65822586909f513ed5bb121d7f4dc243ac74
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-08-02 20:29:39 -0300

    feat: definir port inbound para User

--- internal/port/inbound/user.go
+++ internal/port/inbound/user.go
@@ -1,1 +1,31 @@
 package inbound
+
+import (
+	"context"
+
+	"github.com/marcelofabianov/my-cash/internal/domain"
+)
+
+// PKG
+
+type PasswordHasher interface {
+	Hash(data string) (string, error)
+	Compare(data, encodedHash string) (bool, error)
+}
+
+// User / UseCase
+
+type CreateUserUseCaseInput struct {
+	Document string
+	Name     string
+	Email    string
+	Password string
+}
+
+type CreateUserUseCaseOutput struct {
+	User *domain.User
+}
+
+type CreateUserUseCase interface {
+	Execute(ctx context.Context, input CreateUserUseCaseInput) (*CreateUserUseCaseOutput, error)
+}
