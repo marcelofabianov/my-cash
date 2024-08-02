5b1b90ed99ae3855e98693cd1bd6d5074afe4b7c
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-08-02 20:41:39 -0300

    feat: implementar CreateUser de UserService

--- internal/application/service/user_service.go
+++ internal/application/service/user_service.go
@@ -1,1 +1,32 @@
 package service
+
+import (
+	"context"
+
+	"github.com/marcelofabianov/my-cash/internal/port/inbound"
+	"github.com/marcelofabianov/my-cash/pkg/logger"
+)
+
+type UserService struct {
+	log        logger.Logger
+	createUser inbound.CreateUserUseCase
+}
+
+func NewUserService(log logger.Logger, createUser inbound.CreateUserUseCase) *UserService {
+	return &UserService{log, createUser}
+}
+
+func (s *UserService) CreateUser(ctx context.Context, input inbound.CreateUserServiceInput) (*inbound.CreateUserServiceOutput, error) {
+	output, err := s.createUser.Execute(ctx, input.CreateUserUseCaseInput)
+	if err != nil {
+		return nil, err
+	}
+
+	// Todo: dispatch user.created event
+
+	s.log.Info("user created", s.log.Field("user", output.User))
+
+	return &inbound.CreateUserServiceOutput{
+		CreateUserUseCaseOutput: *output,
+	}, nil
+}
