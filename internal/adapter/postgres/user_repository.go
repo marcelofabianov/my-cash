d0796bd98b459fd72d194d056487fa9fe0bb2095
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-08-02 20:27:14 -0300

    feat: implementar func Create em UserRepository

--- internal/adapter/postgres/user_repository.go
+++ internal/adapter/postgres/user_repository.go
@@ -1,1 +1,52 @@
 package postgres
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/marcelofabianov/my-cash/internal/port/outbound"
+)
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) Create(ctx context.Context, input outbound.CreateUserRepositoryInput) error {
+	query := `
+		INSERT INTO users (id, document, name, email, password, enabled, created_at, updated_at, deleted_at)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+	`
+
+	u := input.User
+
+	result, err := r.db.ExecContext(ctx, query,
+		u.ID.String(),
+		u.Document.String(),
+		u.Name,
+		u.Email.String(),
+		u.Password.String(),
+		u.Enabled.Bool(),
+		u.CreatedAt.String(),
+		u.UpdatedAt.String(),
+		nil,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return err
+	}
+
+	return nil
+}
