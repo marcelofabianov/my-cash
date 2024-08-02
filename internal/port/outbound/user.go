7494cf34778a065b332ea9e8d65909c626e73163
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-08-02 20:24:56 -0300

    feat: definir port outbound para UserRepository

--- internal/port/outbound/user.go
+++ internal/port/outbound/user.go
@@ -1,1 +1,21 @@
 package outbound
+
+import (
+	"context"
+
+	"github.com/marcelofabianov/my-cash/internal/domain"
+)
+
+// User / Repository
+
+type CreateUserRepositoryInput struct {
+	User *domain.User
+}
+
+type CreateUserRepository interface {
+	Create(ctx context.Context, input CreateUserRepositoryInput) error
+}
+
+type UserRepository interface {
+	CreateUserRepository
+}
