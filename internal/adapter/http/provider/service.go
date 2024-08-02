054edba6af0b22101bf5b945b6511856099403cf
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-08-02 20:47:43 -0300

    feat: registrar no service provider o UserService para ficar disponivel storage local da app fiber

--- internal/adapter/http/provider/service.go
+++ internal/adapter/http/provider/service.go
@@ -5,13 +5,33 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 
+	"github.com/marcelofabianov/my-cash/internal/container"
+	"github.com/marcelofabianov/my-cash/internal/port/inbound"
 	"github.com/marcelofabianov/my-cash/pkg/logger"
 )
 
 func ServiceProvider(db *sql.DB, logger logger.Logger) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		//....
+		if err := registerUserService(c, db, logger); err != nil {
+			return err
+		}
 
 		return c.Next()
 	}
+}
+
+func registerUserService(c *fiber.Ctx, db *sql.DB, logger logger.Logger) error {
+	userC := container.NewUserContainer(db, logger)
+
+	var userS inbound.UserService
+	err := userC.Invoke(func(service inbound.UserService) {
+		userS = service
+	})
+	if err != nil {
+		return err
+	}
+
+	c.Locals("userService", userS)
+
+	return nil
 }
