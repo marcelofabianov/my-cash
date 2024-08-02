b94b9544dfe1c832e96db11348782bc58def362a
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-08-02 20:33:18 -0300

    feat: criar error-handler e definir tipos de erros do user

--- internal/domain/error/domain_error.go
+++ internal/domain/error/domain_error.go
@@ -1,1 +1,22 @@
 package error
+
+import (
+	"log"
+
+	"github.com/marcelofabianov/my-cash/config"
+	"github.com/marcelofabianov/my-cash/pkg/logger"
+)
+
+func InitLogger() *logger.Logger {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("error loading config: %v", err)
+	}
+
+	logger, err := logger.NewLogger(cfg.Log)
+	if err != nil {
+		log.Fatalf("error creating logger: %v", err)
+	}
+
+	return logger
+}
--- internal/domain/error/user_error.go
+++ internal/domain/error/user_error.go
@@ -1,1 +1,48 @@
 package error
+
+import (
+	"errors"
+)
+
+var (
+	ErrUserNotCreated            = errors.New("error_user_not_created")
+	ErrUserPasswordHashFailed    = errors.New("error_user_password_hash_failed")
+	ErrUserEmailAlreadyExists    = errors.New("error_user_email_already_exists")
+	ErrUserDocumentAlreadyExists = errors.New("error_user_document_already_exists")
+	ErrUserInvalidEntityData     = errors.New("error_user_invalid_entity_data")
+)
+
+func NewUserNotCreatedError(err error) error {
+	l := InitLogger()
+	l.Error("UserError", l.FieldError(err))
+
+	return ErrUserNotCreated
+}
+
+func NewUserPasswordHashFailedError(err error) error {
+	l := InitLogger()
+	l.Error("UserError", l.FieldError(err))
+
+	return ErrUserPasswordHashFailed
+}
+
+func NewUserEmailAlreadyExistsError() error {
+	l := InitLogger()
+	l.Error("UserError: Email already exists")
+
+	return ErrUserEmailAlreadyExists
+}
+
+func NewUserDocumentAlreadyExistsError() error {
+	l := InitLogger()
+	l.Error("UserError: Document already exists")
+
+	return ErrUserDocumentAlreadyExists
+}
+
+func NewUserInvalidEntityDataError(err error) error {
+	l := InitLogger()
+	l.Error("UserError: Invalid entity data", l.FieldError(err))
+
+	return ErrUserInvalidEntityData
+}
