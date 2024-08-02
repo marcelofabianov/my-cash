d4a203d1747848a561d51bfc1cbe13ebbbb590d1
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-08-02 18:44:17 -0300

    chore: criar pkg para gerir a conexao com banco de dados conforme a config

---
+++ pkg/database/database.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/marcelofabianov/my-cash/config"
+)
+
+type Database struct {
+	conn *sql.DB
+}
+
+func NewDatabase(conn *sql.DB) *Database {
+	return &Database{conn: conn}
+}
+
+func (d *Database) Conn() *sql.DB {
+	return d.conn
+}
+
+func (d *Database) Close() error {
+	return d.conn.Close()
+}
+
+func (p *Database) Ping(ctx context.Context) error {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	return p.conn.PingContext(ctx)
+}
+
+func Connect(ctx context.Context, cfg config.DatabaseConfig) (*Database, error) {
+	dsn := FormatDSN(cfg)
+
+	conn, err := sql.Open(cfg.Driver, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := conn.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return NewDatabase(conn), nil
+}
+
+func FormatDSN(cfg config.DatabaseConfig) string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
