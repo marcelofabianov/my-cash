f77d36b3af2406dfda980eb3e156a290b2e2ffef
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-08-02 21:01:44 -0300

    feat: criar migration para table users

---
+++ db/migrations/20240803000020_create_users_table.sql
@@ -0,0 +1,32 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    password TEXT NOT NULL,
+    document VARCHAR(14) NOT NULL UNIQUE,
+    enabled BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP
+);
+
+CREATE INDEX IF NOT EXISTS idx_users_email ON users (email);
+
+CREATE INDEX IF NOT EXISTS idx_users_document ON users (document);
+
+CREATE INDEX IF NOT EXISTS idx_users_enabled ON users (enabled);
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX IF EXISTS idx_users_enabled;
+
+DROP INDEX IF EXISTS idx_users_document;
+
+DROP INDEX IF EXISTS idx_users_email;
+
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
